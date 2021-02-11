579e98db8db6b608a51ad3e9b8a866d48baaea29
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2021-02-11 19:53:16 -0300

    feat: added eslint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import React from "react";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
 
-export default MyApp
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
